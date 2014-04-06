9f6e3b1592756b090e4d74876fea088455a45dc0
Emma <emma_williams1@hotmail.co.uk>
2014-04-06 14:49:37 +0100

    revised directory

--- directory.rb
+++ directory.rb
@@ -1,4 +1,4 @@
-# let's put all students into an array
+#lets put all students into an array
 students = [
  "The students of my cohort at Makers Academy",
  "Steve Musgrave",
@@ -25,26 +25,8 @@ students = [
 # and then print them
 puts "The students of my cohort at Makers Academy"
 puts "--------------"
-puts students [0]
-puts students [1]
-puts students [2]
-puts students [3]
-puts students [4]
-puts students [5]
-puts students [6]
-puts students [7]
-puts students [8]
-puts students [9]
-puts students [10]
-puts students [11]
-puts students [12]
-puts students [13]
-puts students [14]
-puts students [15]
-puts students [16]
-puts students [17]
-puts students [18]
-puts students [19]
-puts students [20]
+students.each do |student|
+	puts student
+end
 # Finally, we print the total
 puts "Overall, we have #{students.length} great students"
